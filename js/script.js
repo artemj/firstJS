e7db09e28a26563022c66d2265c37308023df639
Artem <artem@gmail.com>
2023-01-22 23:00:10 +0200

    Переписал через циклы

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
 "use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let incr = 10,
-    decr = 10;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// ++incr;
-// --decr;
+for (let i = 0; i < 2; i++){
+    let a = prompt('Один из последних просмотренных фильмов?',''),
+        b = prompt('На сколько оцените его?','');
+   
+    if (a != null && b != null  && a != '' && a.length > 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }   
+}
 
-// console.log(++incr);
-// console.log(--decr);
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
-console.log(incr === decr);
- 
 
+console.log(personalMovieDB);
 
